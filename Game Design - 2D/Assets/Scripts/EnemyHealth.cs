763dc0e913cfe49f3cbb62e8cff579cf901c87e2
Jepp Vad <jbva.skp@edu.mercantec.dk>
2020-06-19 11:27:20 +0200

    Proto-type

---
+++ Game Design - 2D/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+    
+    void OnCollisionEnter2D (Collision2D col) {
+
+        if(col.gameObject.tag.Equals("Bullet")) {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Game Design - 2D/Assets/Scripts/PlayerDie.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDie : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject gameOverText, restartButton;
+    void Start()
+    {
+        gameOverText.SetActive(false);
+        restartButton.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D col) {
+
+        if(col.gameObject.tag.Equals("Enemy")) {
+            gameOverText.SetActive(true);
+            restartButton.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
